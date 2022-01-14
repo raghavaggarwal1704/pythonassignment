d5364ec7310b45dec772e6cb73ff921bf58beb0c
raghavaggarwal1704 <97439575+raghavaggarwal1704@users.noreply.github.com>
2022-01-14 21:39:40 +0530

    Add files via upload

---
+++ asg1_21103046_raghavaggarwal output.py
@@ -0,0 +1,43 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\Raghav\Desktop\asg1_21103046_raghavaggarwal.py =======
+ question 1
+Enter first number:3
+Enyter second number:4
+Enter third number:5
+Average of the three numbers is : 4.0
+
+
+ question 2
+enter your gross income: 100000
+Enter your number of dependents: 3
+Your income tax is : 17399.4
+
+
+ question 3
+Enter your SID:21103046
+Enter your name:raghav aggarwal
+Enter your Gender:male
+Enter your course name:cse
+Enter your CGPA:10
+['21103046', 'raghav aggarwal', 'male', 'cse', 10.0]
+ 
+
+ question 4
+Enter marks of students: 70
+Enter marks of students: 90
+Enter marks of students: 80
+Enter marks of students: 100
+Enter marks of students: 20
+['100', '20', '70', '80', '90']
+ 
+
+ question 5(a)
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+ 
+
+ question 5(b)
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+
