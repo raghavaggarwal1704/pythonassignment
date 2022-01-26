53c7d6482a39a6198505642fb7fdf67c9ab64cc5
raghavaggarwal1704 <97439575+raghavaggarwal1704@users.noreply.github.com>
2022-01-26 20:08:58 +0530

    Add files via upload

---
+++ ass2_21103046_raghavaggarwal(output).py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\Raghav\Desktop\ass2_21103046_raghavaggarwal.py =======
+Question 1
+A Part
+35
+B Part
+egaugnal evitisnes esac a si nohtyP
+C Part
+a case sensitive
+D Part
+Python is object oriented language
+E Part
+10
+F Part
+Pythonisacasesensitivelanguage
+
+
+Question 2
+Hey, Raghav aggarwal Here!
+My SID is 21103046
+I am from CSE department and my CGPA is 9.9
+
+
+Question 3
+a&b: 8
+a|b: 58
+a^b: 50
+Left Shift of a by 2 bits: 224 Left shift of b by 2 bits: 40
+Right Shift of a by 2 bits: 14 Right shift of b by 4 bits: 0
+
+
+Question 4
+Enter 1st Number:
+56
+Enter 2nd Number:
+48
+Enter 3rd Number:
+50
+The Greatest Number is:
+56
+
+
+Question 5
+Enter A String:
+hello
+No
+
+
+Question 6
+Enter The Side 1:
+3
+Enter The Side 2:
+4
+Enter The Side 3:
+5
+If the triangle can be formed?
+Yes
