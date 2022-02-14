ae0c207d090c328b45f42097115aae2ec3353a97
raghavaggarwal1704 <97439575+raghavaggarwal1704@users.noreply.github.com>
2022-02-15 00:17:26 +0530

    Add files via upload

---
+++ 21103046_raghav aggarwal_output.py
@@ -0,0 +1,84 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\aggar\Downloads\21103046_raghavaggarwal_assignment3.py ===
+The solution of Question 1 is:
+
+Enter the string: raman
+The occurence(s) of:
+r is/are 1
+a is/are 2
+m is/are 1
+n is/are 1
+
+The solution of Question 2 is:
+
+Day - 2
+Month - 3
+Year - 2004
+Next Date is:3/3/2004
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Done!
+
+The solution of Question 4 is:
+
+Give a number between 4 and 10 including them- 
+8
+Performance=Very Good
+
+Letter Grade=B+
+
+
+The solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: ram
+Enter the SID of ram:21003
+You have entered 1 values till now
+Do you want to enter more data? y
+Enter the name of the student: game
+Enter the SID of game:21103
+You have entered 2 values till now
+Do you want to enter more data? n
+
+a. Student Details:  
+The SID of ram is  21003
+The SID of game is  21103
+
+b. Student Details (sorted with respect to names):
+The SID of game is 21103
+The SID of ram is 21003
+
+c. Student Details (sorted with respect to SIDs):
+The SID of ram is 21003
+The SID of game is 21103
+
+d. Enter the SID of the student: 21103
+The name of student whose SID is 21103 is game
+Give a number-4
+Give a number-5
+[4, 5, 9]
+Give y to continue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[4, 5, 9]
+Average of the sequence-
+6.0
+Done!
+{1, 3, 5, 6, 8}
+Done!
+{3, 6, 8, 9, 13, 17}
+Done!
+{1, 2, 4, 5}
+Done!
+{6, 7, 8, 9, 10}
+Done!
+{10, 7}
+Done!
