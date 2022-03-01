3b2b422b71b80c5f4798d6a0185a8170ecd87c41
raghavaggarwal1704 <97439575+raghavaggarwal1704@users.noreply.github.com>
2022-03-01 23:33:34 +0530

    Add files via upload

---
+++ 21103046_raghavaggarwal_assignment4output.py
@@ -0,0 +1,75 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\aggar\Downloads\21103046_raghavaggarwal_assignment4.py ===
+Question number 1
+ the tower of Hanoi with three disks.
+Move Disk 1 from U to V
+Move Disk 2 from U to W
+Move Disk 1 from V to W
+Move Disk 3 from U to V
+Move Disk 1 from W to U
+Move Disk 2 from W to V
+Move Disk 1 from U to V
+
+Question number 2
+input value of n: 8
+
+Using recursions:
+
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+
+ Using loops:
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+
+Question number 3
+input  first integer: 5
+input second integer: 7
+Remainder is:  5
+Quotient is:  0
+One value is zero
+{9}
+{9, 10}
+{9, 10, 5}
+{9, 10, 11, 5}
+{5, 6, 9, 10, 11}
+{5, 6, 9, 10, 11}
+Immutable set:  frozenset({5, 6, 9, 10, 11})
+Largest value in the set:  11
+Hash value:  11
+
+Question number 4
+Object has been created
+The name of the student it Raghav and SID is 21103046.
+Object has been destroyed
+
+Question number 5
+part a
+The updated salary of the employee Mehak is 70000
+part b
+Employee Viren record deleted
+
+Question number 6
+Enter the first word: tom
+
+Enter a new meaningful word with the exact same letters : tommy 
+The letters aren't exact, friendship is fake!!
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test?
+
